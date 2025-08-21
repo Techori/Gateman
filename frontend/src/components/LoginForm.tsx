e16d2864bf95e9e3332136dfe90ec4c2a60cfc5b
devHksingh <hksingh.dev@gmail.com>
2025-08-21 07:46:19 +0530

    Add login form

---
+++ frontend/src/components/LoginForm.tsx
@@ -0,0 +1,93 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Link } from "react-router";
+
+const formSchema = z.object({
+  password: z.string().min(6, {
+    message: "password must be at least 6 characters.",
+  }),
+  email: z.email(),
+});
+
+const LoginForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+  return (
+    <div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-6"
+        >
+          <div className="flex flex-col items-center gap-2 text-center">
+            <h1 className="text-3xl font-bold">Login to your account</h1>
+            <p className="text-muted-foreground text-sm text-balance">
+              Enter your email below to login to your account
+            </p>
+          </div>
+          <div className="grid gap-6">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="email" type="email" {...field} />
+                  </FormControl>
+                  <FormDescription>Enter your email id.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="password" type="password" {...field} />
+                  </FormControl>
+                  <FormDescription>Enter your password.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full hover:cursor-pointer">
+              Login
+            </Button>
+          </div>
+        </form>
+      </Form>
+      <div className="text-center text-sm mt-6">
+        Don&apos;t have an account?{" "}
+        <Link    to={'/auth/register'} className="underline underline-offset-4">
+          Sign up
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
